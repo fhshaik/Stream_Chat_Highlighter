ce75123090a1a2ffd0af357d712f5b51e05a2cd5
fhshaik <123507582+fhshaik@users.noreply.github.com>
2024-06-19 21:33:48 -0700

    Added heap to deal with the n most best messages

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sentence_transformers import SentenceTransformer, util
 import heapq
 
 QUEUE_SIZE = 10
+TOP_SIZE = 3
 
 # Load pre-trained SentenceTransformer model
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
@@ -20,8 +21,8 @@ def compute_cosine_similarity(embedding1, embedding2):
     return similarity.item()
 
 # Example sentences
-sentence1 = "Fried Chicken is good food."
-sentence2 = "KFC hits the feels."
+sentence1 = "white"
+sentence2 = "black"
 
 # Measure time to get embeddings
 start_time = time.time()
@@ -42,21 +43,29 @@ messageQueue = deque(maxlen=QUEUE_SIZE)
 userInput = ""
 
 while userInput != "stop":
+    topMessages = []
     userInput = input("Enter message into chat (type 'stop' to exit): ")
     if userInput == "stop":
         break
 
-    inputTuple = [userInput, 0]
+    inputTuple = [0,userInput]
     input_embedding = get_sentence_embedding(userInput)
 
+    remMessage = None
     if len(messageQueue) == QUEUE_SIZE:
-        messageQueue.popleft()
+        remMessage = messageQueue.popleft()[1]
 
     for i in messageQueue:
-        stored_embedding = get_sentence_embedding(i[0])
+        stored_embedding = get_sentence_embedding(i[1])
         similarity = compute_cosine_similarity(stored_embedding, input_embedding)
-        i[1] += similarity
-        inputTuple[1] += similarity
-
+        if remMessage != None:
+            remSimilarity = compute_cosine_similarity(stored_embedding, get_sentence_embedding(remMessage))
+        else:
+            remSimilarity = 0
+        i[0] -= similarity-remSimilarity
+        inputTuple[0] -= similarity
     messageQueue.append(inputTuple)
-    print(messageQueue)
+    messageHeap = list(messageQueue)
+    heapq.heapify(messageHeap)
+    topMessages = list(map(lambda x: x[1], heapq.nsmallest(TOP_SIZE, messageHeap)))
+    print(topMessages)
